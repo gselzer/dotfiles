be08de30e3b2669edaf250c96628998c3ef7fabf
Gabriel Selzer <gjselzer@wisc.edu>
2024-12-09 12:27:34 -0600

    Add (micro)mamba support

--- profile.ps1
+++ profile.ps1
@@ -1,13 +1,28 @@
-# -- Modules -- #
+# -- Micromamba -- #
 
-#oh-my-posh
-oh-my-posh init pwsh | Invoke-Expression
-# git-aliases
-Import-Module git-aliases -DisableNameChecking
-# posh-git
+#region mamba initialize
+# !! Contents within this block are managed by 'mamba shell init' !!
+$Env:MAMBA_ROOT_PREFIX = "C:\Users\gjselzer\micromamba"
+$Env:MAMBA_EXE = "C:\Users\gjselzer\AppData\Local\micromamba\micromamba.exe"
+(& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -p $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
+#endregion
+
+Set-Alias -Name mamba -Value micromamba
+
+mamba config --set changeps1 False
+
+# -- oh-my-posh -- #
+
+oh-my-posh init pwsh --config "C:\Users\gjselzer\code\gselzer\dotfiles\themes\mytheme.omp.json" | Invoke-Expression
+
+# -- posh-git -- #
+
 Import-Module posh-git
-# zoxide
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
-# -- Aliases -- #
-Set-Alias -Name mamba -Value C:\Users\gjselzer\AppData\Local\micromamba\micromamba.exe
+# -- git-aliases -- #
+
+Import-Module git-aliases -DisableNameChecking
+
+# -- zoxide -- # 
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
